1571550e9c01e045344b74e7aef91bed298e0348
Dan Johnston <Zangow@gmail.com>
2017-08-07 20:35:23 -0700

    TODOs 1-8

--- demo.js
+++ demo.js
@@ -8,8 +8,9 @@ function swapPicture() {
 	var match = regex.exec(currentImage.src.substring(currentImage.src.lastIndexOf("/")));  
 	var currentIndex = match[1];
 	currentIndex++;
-	if(currentIndex > 3) {
-		currentIndex = 1;
-	}
+
+	//TODO #8
+	// It seems clicking on the image more than a few times causes it to disappear!
+	// If the currentIndex is greater than 3, lets reset it to 1;
 	currentImage.src = prefix + currentIndex + suffix;
 }
